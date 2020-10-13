755d206affb7472a1e3ec007c93d9ace9062c3fd
Edward <eddieim@gmail.com>
2020-10-13 11:02:11 -0600

    w1d2

---
+++ week_1/day_2/demo.js
@@ -0,0 +1,42 @@
+// VARIABLES
+
+13 // integer
+2.3 // doubles, floats
+'a' //characters
+"edward" //strings
+"123!@#%%#$dfsa" //strings
+true //booleans
+false //booleans
+
+// INITIALIZING A VARIABLE
+var age = 29;
+var name = "edward";
+var double = 3.14;
+
+// MATH OPERATIONS
+// HOW TO ADD
+age = age + 1
+age++
+// HOW TO SUBTRACT
+age = age - 1
+age--
+// MULTIPLICATION AND DIVISION
+age = age / 2
+age = age * 2
+// MODULOUS
+var remainder
+remainder = 45 % 2
+
+// CONSOLE.LOG
+console.log(age)
+console.log(name)
+
+// LOOPS / CONDITIONALS
+for(var index = 0; index < 100; index++){
+    console.log(index)
+}
+
+// 1. INITIAL VARIABLE
+// 2. CONDITION
+// 3. INCREMENT / DECREMENT
+// 4. CODE BODY
