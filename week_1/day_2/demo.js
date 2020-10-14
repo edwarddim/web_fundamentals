3209f84e049057cba1a978c342847e49c958394d
Edward <eddieim@gmail.com>
2020-10-14 10:28:41 -0600

    w1d3

--- week_1/day_2/demo.js
+++ week_1/day_2/demo.js
@@ -33,10 +33,38 @@ console.log(name)
 
 // LOOPS / CONDITIONALS
 for(var index = 0; index < 100; index++){
-    console.log(index)
+    if(index % 2 == 0){ // Is index an even number
+        console.log("Index is an even number") // Index is only being printed when it's an even number
+    }
+    else if(index % 3 == 0){ // Is index divisible by 3
+        console.log("Index is evenly divisible by 3")
+    }
+    else{
+        console.log("Index is an odd number")
+    }
 }
-
 // 1. INITIAL VARIABLE
 // 2. CONDITION
 // 3. INCREMENT / DECREMENT
 // 4. CODE BODY
+
+// CREATE A WHILE LOOP THAT PRINTS 100 TO 0
+var x = 100
+while(x >= 0){
+    x--
+    console.log(x)
+}
+
+
+var i = 0;
+while(i < 100){
+    console.log(i)
+    i++
+}
+
+
+
+
+// CREATE A FOR LOOP THAT PRINTS ODD NUMBERS FROM 1 - 20
+// BONUS: PRINT ODD NUMBERS FROM 1 TO 20 AND PRINT THE SUM OF THE NUMBERS SO FAR
+
